6a928cbc1dfc39114d93182bd69ae1ffaa350d88
ferrypratamaa-00 <ferryaguspratama@gmail.com>
2025-12-28 11:59:11 +0700

    feat: create hooks useSplashScreen

--- apps/mobile/src/routes/index.tsx
+++ apps/mobile/src/routes/index.tsx
@@ -1,10 +1,12 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { SplashScreen } from '@/features/splash-screen/ui/SplashScreen'
+import { createFileRoute } from "@tanstack/react-router";
+import { SplashScreen } from "@/features/splash-screen/ui/SplashScreen";
+import { useSplashScreen } from "@/features/splash-screen/hooks/useSplashScreen";
 
-export const Route = createFileRoute('/')({
-  component: RouteComponent,
-})
+export const Route = createFileRoute("/")({
+    component: RouteComponent,
+});
 
 function RouteComponent() {
-  return <SplashScreen />
+    useSplashScreen({ delay: 2000 });
+    return <SplashScreen />;
 }
