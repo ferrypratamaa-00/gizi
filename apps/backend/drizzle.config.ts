a4031c6177ea7b7a872ba7a4c7dc95f065dd8bcc
ferrypratamaa-00 <ferryaguspratama@gmail.com>
2026-01-01 14:59:02 +0700

    resolved the issue where drizzle-kit generate was finding 0 tables

--- apps/backend/drizzle.config.ts
+++ apps/backend/drizzle.config.ts
@@ -1,7 +1,7 @@
 import { Config } from "drizzle-kit";
 
 export default {
-    schema: "./src/db/schema.ts",
+    schema: "./src/db/schema",
     out: "./drizzle",
     dialect: "postgresql",
     dbCredentials: {
